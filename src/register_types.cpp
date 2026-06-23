5efeeca7f2ded76185cce7dbbd43edcabb094510
M-Senko <112395583+DrBRed@users.noreply.github.com>
2026-06-23 19:38:54 +0700

    first commit

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,5 +1,4 @@
 // Include your classes, that you want to expose to Godot
-#include "item_data.hpp"
 
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
@@ -13,9 +12,6 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-
-	// Register your classes here, so they are available in the Godot editor and engine
-	GDREGISTER_CLASS(ItemData)
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
@@ -27,7 +23,7 @@ void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
 extern "C"
 {
 	// Initialization
-	GDExtensionBool GDE_EXPORT plugin_name_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT cpp_test_plugin_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 		init_obj.register_initializer(initialize_gdextension_types);
